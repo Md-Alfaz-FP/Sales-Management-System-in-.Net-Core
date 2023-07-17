e3e3522d8b1a67fa8a40b2f6b36e02c56520ae57
Md-Alfaz-FP <mdalfazsun@gmail.com>
2023-07-18 00:27:20 +0600

    All file Added

---
+++ SalesInformationSystem/SalesInformationSystem/Models/SaleDetail.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesInformationSystem.Models
+{
+    public class SaleDetail
+    {
+        [Key]
+        public long SaleDetailId { get; set; }
+        public long? SaleId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        [ForeignKey("SaleId")]
+        public virtual SaleMaster SaleMaster { get; set; }
+    }
+}
---
+++ SalesInformationSystem/SalesInformationSystem/Models/SaleMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesInformationSystem.Models
+{
+    public class SaleMaster
+    {
+        [Key]
+        public long SaleId { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string Gender { get; set; }
+        public ICollection<SaleDetail> SaleDetails { get; set; }
+    }
+}
